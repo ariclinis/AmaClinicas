27aea0ca7c0d733f1c7135df05c7cc90a6a58908
ariclinis <ariclinis1@gmail.com>
2018-01-08 16:09:44 +0000

    Tet mt-m2g

--- Controllers/Admin/GestaoFarmacia.php
+++ Controllers/Admin/GestaoFarmacia.php
@@ -182,7 +182,7 @@ class GestaoFarmacia {
         return $this;
     }
 //--------------------------- INSERTES -------------------------//
-public function inserir_venda(PDO $con) {
+public function inserir_venda(PDO $con){
         $stmt = $con->prepare("INSERT INTO `tbl_vendas`(`n_factura`, `cod_cliente`, `valor_factura`, `total`, `descontos`, `valor_recebido`, `troco`, `forma_pagamento`, `data_venda`, `id_utilizador`) VALUES (?,?,?,?,?,?,?,?,?,?)");
         $stmt->execute(
             array(
